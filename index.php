366f90d8d14fda31d6ac852c775e8c3b0ef5216d
Nandz024 <nandhini@karunya.edu.in>
2020-03-04 11:31:29 +0000

    my first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Fluxgen</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="form">
+		<h1>Fluxgen Tech</h1>
+<form method="POST" action="loginapp1.php">
+	<p>
+		<label>Username:</label>
+		<input type="text"  name="uname" placeholder="enter your Username" />
+	</p>
+	<p>
+		<label>Password:</label>
+		<input type="text"  name="password" placeholder="enter your password" />
+	</p>
+	<p>
+		<input type="submit" id="btn"  value="Login" />
+	</p>
+
+</form>
+</div>
+
+</body>
+</html>
+<?php
+session_destroy();
+?>
